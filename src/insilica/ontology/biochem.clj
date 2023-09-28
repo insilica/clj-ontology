2ba16a1206973f3f115b986ed966586872d26540
John Shaffer <john@insilica.co>
2023-09-28 12:04:36 -0500

    Add more classes used in HGNC

--- src/insilica/ontology/biochem.clj
+++ src/insilica/ontology/biochem.clj
@@ -13,15 +13,79 @@
     "http://semanticscience.org/resource/CHEMINF_000446"
     :resource-prefix "https://commonchemistry.cas.org/detail?cas_rn=")
    (clazz
+    "CCDSID"
+    nil
+    "http://identifiers.org/idot:ccdsId"
+    :resource-prefix "http://identifiers.org/ccds/")
+   (clazz
     "Efficacy"
     "The maximum response that can be achieved with the perturbagen as percentage of the positive control."
     "http://www.bioassayontology.org/bao#BAO_0000656"
     :property-type ont/XMLDouble)
    (clazz
+    "EnsemblID"
+    nil
+    "http://edamontology.org/data_2610"
+    :property-type ont/XMLString)
+   (clazz
+    "EntrezID"
+    nil
+    "http://dbpedia.org/ontology/entrezgene"
+    :property-type ont/XMLString)
+   (clazz
+    "Gene"
+    nil
+    "http://dbpedia.org/ontology/gene")
+   (clazz
+    "GeneGroup"
+    nil
+    "http://purl.obolibrary.org/obo/obo:SO0005855"
+    :property-type ont/XMLString)
+   (clazz
+    "GeneLocation"
+    nil
+    "http://dbpedia.org/ontology/GeneLocation"
+    :property-type ont/XMLString)
+   (clazz
+    "GeneLocus"
+    nil
+    "http://purl.jp/bio/4/id/200906015157899027"
+    :property-type ont/XMLString)
+   (clazz
+    "GeneName"
+    "The name of a gene, (typically) assigned by a person and/or according to a naming scheme. It may contain white space characters and is typically more intuitive and readable than a gene symbol. It (typically) may be used to identify similar genes in different species and to derive a gene symbol."
+    "http://edamontology.org/data_2299"
+    :property-type ont/XMLString)
+   (clazz
+    "HGNCID"
+    nil
+    "http://identifiers.org/idot:hgnc"
+    :resource-prefix "http://identifiers.org/hgnc/")
+   (clazz
+    "MeropsID"
+    nil
+    "http://edamontology.org/data_2629"
+    :property-type ont/XMLString)
+   (clazz
+    "MGDMGI"
+    nil
+    "http://purl.obolibrary.org/obo/MI_0479"
+    :property-type ont/XMLString)
+   (clazz
+    "OmimID"
+    nil
+    "http://dbpedia.org/ontology/omim"
+    :property-type ont/XMLString)
+   (clazz
     "ProtocolName"
     nil
     "http://example.com/ProtocolName"
     :property-type ont/XMLString)
+   (clazz
+    "PubmedID"
+    nil
+    "http://identifiers.org/idot:pubmedId"
+    :resource-prefix "http://identifiers.org/pubmed/")
    (clazz
     "PubchemCID"
     nil
@@ -32,6 +96,16 @@
     nil
     "http://identifiers.org/idot:pubchem.substance"
     :resource-prefix "http://identifiers.org/pubchem.substance/")
+   (clazz
+    "RefSeqAccession"
+    "Accession number of a RefSeq database entry."
+    "http://edamontology.org/data_1098"
+    :property-type ont/XMLString)
+   (clazz
+    "RGDID"
+    nil
+    "http://identifiers.org/idot:rgdId"
+    :resource-prefix "http://identifiers.org/rgd/")
    (clazz
     "SampleDataID"
     nil
@@ -51,6 +125,26 @@
     "SMILES"
     nil
     "http://edamontology.org/format_1196"
+    :property-type ont/XMLString)
+   (clazz
+    "Symbol"
+    nil
+    "http://dbpedia.org/ontology/symbol"
+    :property-type ont/XMLString)
+   (clazz
+    "UCSCID"
+    nil
+    "http://example.com/UCSCID"
+    :property-type ont/XMLString)
+   (clazz
+    "UniprotID"
+    nil
+    "http://dbpedia.org/ontology/uniprot"
+    :resource-prefix "http://identifiers.org/uniprot/")
+   (clazz
+    "VegaID"
+    nil
+    "http://example.com/VegaID"
     :property-type ont/XMLString)])
 
 (def-classes classes)
